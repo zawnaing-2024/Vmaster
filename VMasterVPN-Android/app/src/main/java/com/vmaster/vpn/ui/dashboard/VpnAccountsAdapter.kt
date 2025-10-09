473db6e49fc8ad9fb15d5303294d6c2404954a16
Zaw Naing Htun <zawnainghtun@Zaws-MacBook-Pro.local>
2025-10-09 22:47:34 +0700

    Add missing Toast import in VpnAccountsAdapter

--- VMasterVPN-Android/app/src/main/java/com/vmaster/vpn/ui/dashboard/VpnAccountsAdapter.kt
+++ VMasterVPN-Android/app/src/main/java/com/vmaster/vpn/ui/dashboard/VpnAccountsAdapter.kt
@@ -2,6 +2,7 @@ package com.vmaster.vpn.ui.dashboard
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView

ecf79edcc4b54d4c328fc727bdb243f399c07de1
Zaw Naing Htun <zawnainghtun@Zaws-MacBook-Pro.local>
2025-10-10 12:04:38 +0700

    Fix customer expiration migration - remove dependency on max_plan_duration column

--- database/add_customer_expiration.sql
+++ database/add_customer_expiration.sql
@@ -2,6 +2,6 @@
 -- This limits how long a customer account itself is active
 
 ALTER TABLE customers
-ADD COLUMN plan_duration INT NULL COMMENT 'Customer account plan duration in months' AFTER max_plan_duration,
-ADD COLUMN expires_at TIMESTAMP NULL COMMENT 'When the customer account expires' AFTER plan_duration;
+ADD COLUMN plan_duration INT NULL COMMENT 'Customer account plan duration in months',
+ADD COLUMN expires_at TIMESTAMP NULL COMMENT 'When the customer account expires';
 
--- database/add_customer_plan_duration_limit.sql
+++
@@ -1,6 +0,0 @@
--- Add plan duration limit for customers
--- This limits how long VPN accounts can be created for each customer
-
-ALTER TABLE customers
-ADD COLUMN max_plan_duration INT NULL COMMENT 'Maximum VPN plan duration in months for this customer (NULL = unlimited)' AFTER max_total_vpn_accounts;
-
